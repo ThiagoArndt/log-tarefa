c8b651a2d3dfcac8b198d698d571d10445b23148
ThiagoArndt <thiagoarndt04@gmail.com>
2024-08-15 22:02:16 -0300

    Done

--- modules/logger.js
+++ modules/logger.js
@@ -1,13 +1,30 @@
-import winston from "winston";
+import { createLogger, transports, format } from "winston"
+import LokiTransport from "winston-loki"
 
-export const logger = winston.createLogger({
-    level: 'debug',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
 
-        new winston.transports.File({ filename: 'combined.log' }),
-    ],
-});
+let logger
 
-export default logger;
+const initializeLogger = () => {
+    if (logger) {
+        return
+    }
+
+    logger = createLogger({
+        transports: [new LokiTransport({
+            host: "<YOUR API URL GOES HERE (FROM PREVIOUS STEP)>",
+            labels: { app: 'honeyshop' },
+            json: true,
+            format: format.json(),
+            replaceTimestamp: true,
+            onConnectionError: (err) => console.error(err)
+        }),
+        new transports.Console({
+            format: format.combine(format.simple(), format.colorize())
+        })]
+    })
+}
+
+export const getLogger = () => {
+    initializeLogger()
+    return logger
+}
